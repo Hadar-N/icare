102a26c706ae4c31f649c3bbe9dba79924023643
HadarN <hadarnv1@gmail.com>
2024-11-21 17:23:09 +0800

    updated utils

--- utils/setup_helpers.py
+++ utils/setup_helpers.py
@@ -1,4 +1,4 @@
-# from screeninfo import get_monitors
+from screeninfo import get_monitors
 import os
 import pygame
 import cv2
@@ -7,18 +7,27 @@ import numpy as np
 import re
 from utils.consts import IMAGE_RESIZE_WIDTH, WINDOW_WIDTH, MIN_FRAME_CONTENT_PARTITION, SAME_PROPORTIONS_THRESHOLD, TRIM_EDGES_CONST
 
-pattern = re.compile("(?<=\=)\d+\.\d+")
+temp_re = re.compile("(?<=\=)\d+\.\d+")
+diskspace_re = re.compile("[\d.]+(?=%)")
 
 def get_pi_temp ():
     t = os.popen('vcgencmd measure_temp').readline()
-    match = float(re.search(pattern, t)[0])
+    match = float(re.search(temp_re, t)[0])
     return match
 
-def get_secondary_monitor():
-    # monitors = get_monitors()
-    # if len(monitors) > 1:
-    #     return monitors[1]
-    return None
+def get_monitor_information(logger):
+    try:
+        monitors = get_monitors()
+        monitor = monitors[0]
+        is_main = True
+        if len(monitors) > 1:
+            print("monitors 1", monitors[1])
+            is_main = False
+            monitor = monitors[1]
+        return (monitor, is_main)
+    except:
+        logger.warning("no monitor found!")
+        return None, None
 
 def originImageResize(img):
     img_h, img_w, _ = img.shape
@@ -28,86 +37,72 @@ def originImageResize(img):
 
     return img_size
 
-def screenSetup(img_size):
+def screenSetup(img_size, logger):
     window_width = WINDOW_WIDTH
     window_height = window_width*(img_size[1]/img_size[0])
     screen_x = 0
     screen_y = 0
-    flags= 0
 
-    secondary_monitor = get_secondary_monitor()
-    if secondary_monitor:
-        window_height = secondary_monitor.height
-        window_width = secondary_monitor.width
-
-        screen_x = secondary_monitor.x
-        screen_y = secondary_monitor.y
-
-        flags = pygame.FULLSCREEN
-
-        os.environ['SDL_VIDEO_WINDOW_POS'] = f"{screen_x},{screen_y}"
+    monitor, is_main = get_monitor_information(logger)
+    if monitor:
+        window_width = monitor.width
+        window_height = monitor.height
+        if not is_main:
+            screen_x = monitor.x
+            screen_y = monitor.y
+            os.environ['SDL_VIDEO_WINDOW_POS'] = f"{screen_x},{screen_y}"
     else:
-        flags = pygame.FULLSCREEN
-        print("Secondary monitor not found")
+        print("No monitor found!!!")
 
+    flags = pygame.FULLSCREEN
     window_size = (int(window_width), int(window_height))
     return (window_size, flags)
 
-def calcResizeProportion(img_resize, screen_size):
-    # since we warp the image into the screen proportions, but still want to keep it small enough for quick calculation and analysis, we calculate the difference
-    proportion = screen_size[0]/img_resize[0]
-    return proportion, (int(screen_size[0]/proportion), int(screen_size[1]/proportion))
+def sortPoints(points):
+    print("start: ", points)
+    points = np.array(points)
+    sums = points.sum(axis=1)  # x + y
+    diffs = np.diff(points, axis=1)[:, 0]
+
+    sorted_points = np.zeros((4, 2), dtype=np.float32)
+    sorted_points[0] = points[np.argmin(sums)]  # Top-left
+    sorted_points[1] = points[np.argmax(diffs)]  # Bottom-left
+    sorted_points[2] = points[np.argmax(sums)]  # Bottom-right
+    sorted_points[3] = points[np.argmin(diffs)]  # Top-right
+
+    if math.dist(sorted_points[0], sorted_points[1]) < math.dist(sorted_points[0], sorted_points[3]):
+        sorted_points = [sorted_points[0],sorted_points[3], sorted_points[2], sorted_points[1]]
+
+    return sorted_points
 
 def getTransformationMatrix(contours, img_resize, win_size):
-    _, appx, _ = findBoard(contours, img_resize)
-
-    avg_pt_appx = (appx[0][0] + appx[1][0] + appx[2][0] + appx[3][0])/4
-    first_item = next((i for i, x in enumerate(appx) if x[0][0] < avg_pt_appx[0] and x[0][1] > avg_pt_appx[1]), -1)
-    ordered_apps = []
-    for i in range(0,4):
-        ordered_apps.append(appx[(first_item - i)%4])
+    appx = sortPoints([item[0] for item in findBoard(contours, img_resize)])
     
-    trimmed = tripRectEdges(ordered_apps, avg_pt_appx)
-    
-    inp =  np.float32([trimmed[0], trimmed[1], trimmed[2], trimmed[3]])
-    out =  np.float32([[0, win_size[1] - 1], [0,0], [win_size[0] - 1, 0], [win_size[0] - 1, win_size[1] - 1]])
+    inp =  np.float32(appx)
+    out =  np.float32([[0,0], [0, win_size[0] - 1], [win_size[1] - 1, win_size[0] - 1], [win_size[1] - 1, 0]])
 
-    inp_proportion = getRectProportions(inp)
-    out_proportion = getRectProportions(out)
+    # inp_proportion = getRectProportions(inp)
+    # out_proportion = getRectProportions(out)
 
-    new = inp
+    # new = inp
 
-    if min(inp_proportion/out_proportion, out_proportion/inp_proportion) < SAME_PROPORTIONS_THRESHOLD:
-    # after organizing the spots, we can assume inp[0],inp[1] on the left end and inp[2],inp[3] on the right end, therefore x(inp[0],inp[1]) > x(inp[2],inp[3])
-        dist_no_change = math.dist(inp[0],inp[1])
-        top_proportion = math.dist(inp[1],inp[2]) / (math.dist(inp[3],inp[0]) + math.dist(inp[1],inp[2]))
+    # if min(inp_proportion/out_proportion, out_proportion/inp_proportion) < SAME_PROPORTIONS_THRESHOLD:
+    #     dist_no_change = math.dist(inp[0],inp[1])
+    #     top_proportion = math.dist(inp[1],inp[2]) / (math.dist(inp[3],inp[0]) + math.dist(inp[1],inp[2]))
 
-        new_point_2 = findPointOnLine(inp[1], inp[2], dist_no_change*out_proportion*top_proportion*2)
-        new_point_3 = findPointOnLine(inp[0], inp[3], dist_no_change*out_proportion*(1-top_proportion)*2)
+    #     new_point_2 = findPointOnLine(inp[1], inp[2], dist_no_change*out_proportion*top_proportion*2)
+    #     new_point_3 = findPointOnLine(inp[0], inp[3], dist_no_change*out_proportion*(1-top_proportion)*2)
 
-        new = np.array((inp[0], inp[1], new_point_2, new_point_3), dtype=np.float32).reshape((-1, 1, 2))
+    #     new = np.array((inp[0], inp[1], new_point_2, new_point_3), dtype=np.float32).reshape((-1, 1, 2))
 
-    matrix = cv2.getPerspectiveTransform(new,out)
+    matrix = cv2.getPerspectiveTransform(inp, out)
 
-    return new, matrix
-
-def tripRectEdges(rect, avg_pt):
-    trimmed = []
-    for i in range(0,4):
-        orig_pt = rect[i][0]
-        for j in range(0,2):
-            if orig_pt[j] < avg_pt[j]: orig_pt[j]+=TRIM_EDGES_CONST
-            else: orig_pt[j]-=TRIM_EDGES_CONST
-        trimmed.append(orig_pt)
-
-    return trimmed
-
+    return inp, out, matrix
 
 def getRectProportions(rect):
     side1 = (math.dist(rect[0],rect[1]) + math.dist(rect[2],rect[3]))/2
     side2 = (math.dist(rect[1],rect[2]) + math.dist(rect[0],rect[3]))/2
-
-    return (side2/side1)
+    return min(side2/side1, side1/side2)
 
 def findPointOnLine(pt1,pt2,dist):
     direction = pt2 - pt1
@@ -121,20 +116,19 @@ def findPointOnLine(pt1,pt2,dist):
 
 def findBoard(conts, img_resize):
     rects = []
+
     fake_contour = np.array([[img_resize[0] - 1, 0], [0,0], [0, img_resize[1] - 1], [img_resize[0] - 1, img_resize[1] - 1]]).reshape((-1,1,2)).astype(np.int32)
     full_area = cv2.contourArea(fake_contour)
     area_theshold = full_area/MIN_FRAME_CONTENT_PARTITION
 
-    for contour in conts:
-        cnt_area = cv2.contourArea(contour)
-        if cnt_area > area_theshold and cnt_area < full_area:
-            epsilon = 0.1 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, epsilon, True)
-            if len(approx) == 4:
-                rects.append((cnt_area, approx, contour))
+    for c in conts:
+        epsilon = 0.02 * cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, epsilon, True)
+        area= cv2.contourArea(approx)
+        if len(approx) == 4 and area > area_theshold and area < full_area:
+            rects.append({"cnt": approx, "area": area})
 
-    if len(rects) == 0:
-        return (full_area, fake_contour, fake_contour)
-    if len(rects) == 1: return rects[0]
-    else:
-        return max(rects, key=lambda r: r[0])
+    match len(rects):
+        case 0: return fake_contour
+        case 1: return rects[0]["cnt"]
+        case _: return max(rects, key=lambda c: c["area"])["cnt"]
