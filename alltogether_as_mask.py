f9e81a5cbf77f4898120da9ffa2857ec78f5118b
HadarN <hadarnv1@gmail.com>
2024-12-05 19:20:02 +0800

    added movement and dotenv

--- alltogether_as_mask.py
+++ alltogether_as_mask.py
@@ -5,6 +5,7 @@ import numpy as np
 import datetime
 import time
 import logging
+from dotenv import load_dotenv
 from picamera2 import Picamera2
 
 import utils.consts as consts
@@ -13,7 +14,9 @@ from utils.setup_helpers import asstr, screenSetup, getTransformationMatrix, ori
 from utils.internals_management_helpers import AddSpritesToGroup, checkCollision, getFishOptions
 from utils.vocab_management_helpers import initVocabOptions, AddVocabToGroup, vocabReadMaskCollision, presentNewZHVocab, vocabMatching
 
-os.environ["DISPLAY"] = ":0"
+load_dotenv(verbose=True, override=True)
+
+os.environ["DISPLAY"] = f':{os.getenv("DISPLAY")}'
 
 def renewCameraPicture(counter, mask, area, mask_img, image):
     if (counter%(consts.CLOCK/2) == 0 or not mask or not area):
@@ -33,16 +36,19 @@ def createMask(current_image):
     current_blurred = cv2.GaussianBlur(current_image, consts.BLUR_SIZE, 0)
     difference = cv2.absdiff(current_blurred, reference_blur)
     gray_image = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
-    _, thresholded = cv2.threshold(gray_image, consts.THRESHOLD_VAL, consts.THRESHOLD_MAX, cv2.THRESH_BINARY_INV)
+    _, thresholded = cv2.threshold(gray_image, threshvalue, consts.THRESHOLD_MAX, cv2.THRESH_BINARY_INV)
     closed = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
-
     return closed
+
+def findthreshval(empty_image):
+    val = np.mean(np.mean(empty_image, axis=(0,1)), axis=0)
+    return val
 
 logging.basicConfig(filename=consts.LOGFILE)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-logger.info(f'--------------start datetime: {datetime.datetime.now()}')
+logger.info(f'--------------start datetime: {datetime.datetime.now()} running on: {os.getenv("ENV")}')
 
 camera = Picamera2()
 
@@ -76,6 +82,8 @@ time.sleep(2)  # Wait for 2 seconds
 kernel = np.ones((11, 11), np.uint8)  # Larger kernel for more aggressive closing
 reference_image = cv2.flip(cv2.warpPerspective(cv2.resize(camera.capture_array(), img_resize), matrix, (global_data.window_size[1], global_data.window_size[0]) ,flags=cv2.INTER_LINEAR), 0)
 reference_blur = cv2.GaussianBlur(reference_image, consts.BLUR_SIZE, 0)
+threshvalue = findthreshval(reference_blur) * 1.2
+print("threshvalue: ", findthreshval(reference_blur), threshvalue)
 
 initVocabOptions()
 vocabengroup = pygame.sprite.Group()
