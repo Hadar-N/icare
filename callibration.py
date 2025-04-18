b9f1653d6c2a8e84637c734fc33608d5e9830bed
Hadar-N <hadarnv1@gmail.com>
2025-04-18 23:04:12 +0800

    callibration working proto

--- callibration.py
+++ callibration.py
@@ -9,9 +9,19 @@ from utils.consts import CAMERA_RES, FONT_PATH, FONT_SIZE, CLOCK
 from utils.helper_functions import setCameraFunction
 
 load_dotenv(verbose=True, override=True)
+env = os.getenv("ENV")
+
+pygame.init()
+
+screen_size = [int(i / 2) for i in CAMERA_RES]
+take_picture, remove_camera = setCameraFunction(env, screen_size[::-1])
+image = take_picture()
+boundaries = pygame.sprite.Group()
+
+BOUNDARY_RADIUS = 5
 
 class BtnItem():
-    def __init__(self, txt, item_no, font, screen_size):
+    def __init__(self, txt, item_no, font):
         self.__rect = pygame.Rect(screen_size[0]/2 - 50, screen_size[1]/2 + item_no*50, 100, 50)
         self.__color = (200,200,200)
         self.__content = txt
@@ -29,41 +39,43 @@ class BtnItem():
         y_colliding = pos[1] > self.__rect[1] and pos[1] < self.__rect[1] + self.__rect[3]
         return x_colliding and y_colliding
 
-def refresh_action(take_picture):
-    global image
-    global boundaries
-    image = take_picture()
-    boundaries= []
+class Boundary(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.center_point = pos
+        self.image = pygame.Surface(screen_size, flags=pygame.SRCALPHA)
+        pygame.draw.circle(self.image, (255,0,0), pos, BOUNDARY_RADIUS)
+        self.rect = self.image.get_rect()
 
+def refresh_action() :
+    globals()["image"] = take_picture()
+    globals()["boundaries"].empty()
 
 def run_calibration():
-    screen_size = [int(i / 2) for i in CAMERA_RES]
-
-    boundaries = []
-    take_picture, remove_camera = setCameraFunction(os.getenv("ENV"), screen_size[::-1])
+    global boundaries
+    global image
 
     os.environ["DISPLAY"] = ":10"
-    pygame.init()
     screen = pygame.display.set_mode(screen_size)
     clock = pygame.time.Clock()
-
-    image = take_picture()
+    
     btns_texts = {
-        "refresh": lambda: refresh_action(take_picture),
-        "submit": lambda: save_and_launch_game(boundaries)
+        "refresh": refresh_action,
+        "submit": save_and_launch_game
     }
     font = pygame.font.Font(FONT_PATH, int(FONT_SIZE/1.5))
 
-    rects = [BtnItem(list(btns_texts.keys())[i], i, font, screen_size) for i in range(0, len(btns_texts))]
+    rects = [BtnItem(list(btns_texts.keys())[i], i, font) for i in range(0, len(btns_texts))]
 
     running = True
     while running:
         screen.fill((0,0,0))
         screen.blit(pygame.surfarray.make_surface(image), (0,0))
         [r.draw_btn(screen) for r in rects]
+        boundaries.draw(screen)
 
-        pygame.display.update()
         clock.tick(CLOCK)
+        pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -72,18 +84,17 @@ def run_calibration():
                 pos=pygame.mouse.get_pos()
                 collided = next((r for r in rects if r.is_colliding(pos)), None)
                 if collided:
-                    # print("collided!!!", collided, collided.content)
                     btns_texts[collided.content]()
                 else:
-                    print (f"x = {pos[0]}, y = {pos[1]}", boundaries)
+                    boundaries.add(Boundary(pos))
 
     pygame.quit()
     sys.exit()
     remove_camera()
 
 
-def save_and_launch_game(boundaries):
-    print("save_and_launch_game: ", boundaries)
+def save_and_launch_game():
+    print("save_and_launch_game: ", globals()["boundaries"])
 
 if __name__ == "__main__":
     run_calibration()
